21340d0635eee2eea0902ce8642f9d59d124a835
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2025-05-27 23:27:33 +0900

    backend init

---
+++ booktrace/src/main/java/com/database/booktrace/BooktraceApplication.java
@@ -0,0 +1,13 @@
+package com.database.booktrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktraceApplication.class, args);
+	}
+
+}
---
+++ booktrace/src/test/java/com/database/booktrace/BooktraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.booktrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
